e1c1a5c3078731874bffe9688a8f4609d6424a4d
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-05-12 21:40:10 +0530

    reverse

---
+++ exercise/src/com/exercise/reverse/ReverseString.java
@@ -0,0 +1,12 @@
+package com.exercise.reverse;
+
+public class ReverseString {
+	public static void main(String[]args) {
+		
+		StringBuffer str=new StringBuffer("DHAYA");
+		str.reverse();
+		System.out.println(str);
+		
+	}
+
+}
